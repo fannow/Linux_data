320e88fcaf22ebeee6ff5f3131dd651035904dc9
fannow <74126398+fannow@users.noreply.github.com>
2022-05-16 21:14:26 +0800

    log

---
+++ LOG_WINDOWS/日志/until.h
@@ -0,0 +1,13 @@
+//#ifndef __UNTIL_H__
+//#define __UNTIL_H__
+//#include<pthread.h>
+//#include<sys/types.h>
+//#include<unistd.h>
+//#include<sys/syscall.h>
+//#include<iostream>
+//using namespace std;
+////获取线程id
+//pid_t GetThreadId() {
+//	return syscall(SYS_gettid);
+//}
+//#endif // !__UNTIL_H__
